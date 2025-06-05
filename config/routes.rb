0e4606665cfe891082ff37d5d2d165a2b9205a30
exec zsh <admir.kurman@gmail.com>
2025-06-05 12:59:10 +0100

    About page completed 1.0

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: "pages#home"
+  get "about", to: "pages#about"
   resources :bands
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
