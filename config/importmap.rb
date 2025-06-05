80a4d905d7ff550587531e36ea90eda834a2d5a9
Milo Vingoe <milovingoe@hotmail.com>
2025-06-05 14:54:17 +0100

    Mapbox added to index page

--- config/importmap.rb
+++ config/importmap.rb
@@ -7,3 +7,5 @@ pin "@hotwired/stimulus-loading", to: "stimulus-loading.js"
 pin_all_from "app/javascript/controllers", under: "controllers"
 pin "bootstrap" # @5.3.6
 pin "@popperjs/core", to: "@popperjs--core.js" # @2.11.8
+pin "mapbox-gl", to: "https://ga.jspm.io/npm:mapbox-gl@3.1.2/dist/mapbox-gl.js"
+pin "process", to: "https://ga.jspm.io/npm:@jspm/core@2.1.0/nodelibs/browser/process-production.js"
