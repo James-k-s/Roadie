0a3b402967524128428a1014c34353faaf004aa2
Milo Vingoe <milovingoe@hotmail.com>
2025-06-08 20:21:28 +0100

    Users can update their profile details

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,14 @@
 class ApplicationController < ActionController::Base
   before_action :authenticate_user!
   before_action :set_user_band
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+  protected
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:username, :first_name, :last_name, :address, :bio, :photo])
+    devise_parameter_sanitizer.permit(:account_update, keys: [:username, :first_name, :last_name, :address, :bio, :photo])
+  end
 
   private
 
