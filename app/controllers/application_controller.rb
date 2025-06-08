fafe4e9498528d0c5e7a1d411e4df35e70c83b37
Milo Vingoe <milovingoe@hotmail.com>
2025-06-08 19:10:41 +0100

    Sign up form columns added

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,13 @@
 class ApplicationController < ActionController::Base
+  before_action :configure_permitted_parameters, if: :devise_controller?
   before_action :authenticate_user!
   before_action :set_user_band
+
+  protected
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:username, :first_name, :last_name, :address, :bio, :photo])
+  end
 
   private
 
