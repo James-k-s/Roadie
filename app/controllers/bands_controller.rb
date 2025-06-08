cbeb660bcdb7960318f2a35aba4061374c6d0a07
Milo Vingoe <milovingoe@hotmail.com>
2025-06-08 23:36:49 +0100

    Users can now create a band

--- app/controllers/bands_controller.rb
+++ app/controllers/bands_controller.rb
@@ -1,4 +1,5 @@
 class BandsController < ApplicationController
+  before_action :set_user, only: [:new, :create]
   def index
     @bands = Band.all
     @markers = @bands.geocoded.map do |band|
@@ -18,5 +19,25 @@ class BandsController < ApplicationController
   def new
     @user = current_user
     @band = Band.new
+  end
+
+  def create
+    @band = Band.new(band_params)
+    @band.user = @user
+    if @band.save
+      redirect_to band_path(@band), notice: 'Band was successfully created.'
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def set_user
+    @user = current_user
+  end
+
+  def band_params
+    params.require(:band).permit(:name, :address, :bio, :genre, :photo)
   end
 end
