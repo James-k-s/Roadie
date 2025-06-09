02d199b49a687cf9f6970691649ae8db98e74f07
Milo Vingoe <milovingoe@hotmail.com>
2025-06-09 16:18:42 +0100

    Chat feature finished

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2025_06_09_135523) do
+ActiveRecord::Schema[7.1].define(version: 2025_06_09_144644) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -163,6 +163,15 @@ ActiveRecord::Schema[7.1].define(version: 2025_06_09_135523) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["band_id"], name: "index_projects_on_band_id"
+  end
+
+  create_table "solid_cable_messages", force: :cascade do |t|
+    t.text "channel"
+    t.text "payload"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["channel"], name: "index_solid_cable_messages_on_channel"
+    t.index ["created_at"], name: "index_solid_cable_messages_on_created_at"
   end
 
   create_table "songs", force: :cascade do |t|
