0e0f5e81e929b6026aff0823f9b7463f0de6e4ab
Milo Vingoe <milovingoe@hotmail.com>
2025-06-09 11:08:20 +0100

    Mapbox now working for users

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,13 @@
 class UsersController < ApplicationController
   def index
     @user = User.all
+    @users = User.all
+    @markers = @user.map do |user|
+      {
+        lat: user.latitude,
+        lng: user.longitude
+      }
+    end
   end
 
   def show
